60595d20cb4df2a190dd2e1c6a3f5a39d13c16b4
ShyroVO <bosquet.laurie@outlook.com>
2020-12-01 11:10:38 +0100

    1.1

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ let pasVieux = " le capitaine n'est pas tres vieux";
 let vieux = " le capitaine est vieux";
 let qui = "Le capitaine a ";
 
-for ( age = 13 ; age <= 70 ; age++ ) {
+for ( age = 13 ; age <= 60 ; age++ ) {
     if (age < 18) {
-        document.getElementById('monDiv').innerHTML+= qui + age++ + "ans," + jeune +"<br>";
+        document.getElementById('monDiv').innerHTML+= qui + age + "ans," + jeune +"<br>";
     }
 
     if (age >= 18 && age <= 50 ){
         document.getElementById('monDiv').innerHTML+= qui + age++ + "ans," + pasVieux +"<br>";
     }
 
-    if (age > 50 && age < 70 ){
-        document.getElementById('monDiv').innerHTML+= qui + age++ + "ans," + vieux +"<br>";
+    if (age > 50 ){
+        document.getElementById('monDiv').innerHTML+= qui + age + "ans," + vieux +"<br>";
     }
 
 }
