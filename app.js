077235895e089570916f86ee92a702a7aa31b94f
ShyroVO <bosquet.laurie@outlook.com>
2020-12-01 11:05:57 +0100

    1.0

---
+++ app.js
@@ -0,0 +1,20 @@
+let age;
+let jeune = " le capitaine est jeune";
+let pasVieux = " le capitaine n'est pas tres vieux";
+let vieux = " le capitaine est vieux";
+let qui = "Le capitaine a ";
+
+for ( age = 13 ; age <= 70 ; age++ ) {
+    if (age < 18) {
+        document.getElementById('monDiv').innerHTML+= qui + age++ + "ans," + jeune +"<br>";
+    }
+
+    if (age >= 18 && age <= 50 ){
+        document.getElementById('monDiv').innerHTML+= qui + age++ + "ans," + pasVieux +"<br>";
+    }
+
+    if (age > 50 && age < 70 ){
+        document.getElementById('monDiv').innerHTML+= qui + age++ + "ans," + vieux +"<br>";
+    }
+
+}
